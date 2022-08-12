a1f92eec73db0d9f289cc6f6714ba2638ff4b2d1
VashuRaj <111109980+VashuRaj@users.noreply.github.com>
2022-08-12 14:03:20 +0530

    Add files via upload

---
+++ sample_info.py
@@ -0,0 +1,24 @@
+# Bot information
+SESSION = 'Media_search'
+USER_SESSION = 'User_Bot'
+API_ID = 12345
+API_HASH = '0123456789abscdef0123456789abcdef'
+BOT_TOKEN = '123456:ABC-DEF1234gchIkl-zyx57W2v1u123ew11'
+USERBOT_STRING_SESSION = ''
+
+# Bot settings
+CACHE_TIME = 300
+USE_CAPTION_FILTER = False
+
+# Admins, Channels & Users
+ADMINS = [123457890, 'admin10023', 090807065432]
+CHANNELS = [-10012345678000, -1009876543210000, 'channelusername']
+AUTH_USERS = []
+AUTH_CHANNEL = None
+
+# MongoDB information
+DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
+DATABASE_NAME = 'Telegram'
+COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
+
+
